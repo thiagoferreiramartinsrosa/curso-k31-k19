2c126bb4c08df4fb6e4cdfd1928ab21e50211096
thiagoferreiramartinsrosa <thiagoferreiramartinsrosa@gmail.com>
2015-07-04 16:41:30 -0300

    Generic

---
+++ Generic/TestaAdicionaNoFinal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+
+public class TestaAdicionaNoFinal
+{
+	static void Main()
+	{
+		List<int> arrayList = new List<int>();
+
+		long tempo = TestaAdicionaNoFinal.AdicionaNoFinal(arrayList);
+		Console.WriteLine("ArrayList: " + tempo + "ms");
+
+		LinkedList<int> linkedList = new LinkedList<int>();
+
+		tempo = TestaAdicionaNoFinal.AdicionaNoFinal(linkedList);
+		Console.WriteLine("LinkedList: " + tempo + "ms");
+	}
+
+	public static long AdicionaNoFinal (ICollection<int> lista)
+	{
+		Stopwatch sw = new Stopwatch();
+
+		sw.Start();
+		int size
+	}
+	}
+}
+
